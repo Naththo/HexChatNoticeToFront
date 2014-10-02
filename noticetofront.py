8ad6ea8fff07afe5b736f7a698c7bd0bdd585e55
Naththo <nathan-thomas@hotmail.com>
2014-10-03 00:19:17 +0100

    Fixed only outputting the first word

--- noticetofront.py
+++ noticetofront.py
@@ -11,7 +11,8 @@ def processNotice(word, word_eol, userdata):
 		return hexchat.EAT_NONE
 
 	nick = word[0].split("!")[0].replace(":", "")
-	noticecontent = word[3][1:len(word[3])]
+	#noticecontent = word[3][1:len(word[3])]
+	noticecontent = getnoticetext(word)
 	if nick and noticecontent:
 		notice_network = hexchat.get_info("network")
 		notice_host = hexchat.get_info("host")
@@ -22,6 +23,21 @@ def processNotice(word, word_eol, userdata):
 			current_tab.emit_print("Notice", nick, "(%s / %s): %s" % (notice_network, notice_host, noticecontent))
 	return hexchat.EAT_ALL
 hexchat.hook_server("NOTICE", processNotice)
+
+def getnoticetext(noticedata):
+	current_word = 3
+	notice_content = []
+	while current_word < len(noticedata):
+		if noticedata[current_word] == "":
+			continue
+
+		if noticedata[current_word][0] == ":":
+			noticedata[current_word] = noticedata[current_word][1:]
+
+		notice_content.append(noticedata[current_word])
+		current_word = current_word + 1
+
+	return ' '.join(notice_content)
 
 def addon_unloaded(userdata):
 	print "\0034 %s (%s) unloaded." % (__module_name__, __module_version__)
