ec66a0294e1cb8e5fd4837fea00997d191ce3848
Naththo <nathan-thomas@hotmail.com>
2014-10-03 01:02:47 +0100

    Updated version

--- noticetofront.py
+++ noticetofront.py
@@ -1,7 +1,7 @@
 import hexchat
 
 __module_name__ = "Notice to front"
-__module_version__ = "0.1"
+__module_version__ = "0.2"
 __module_description__ = "Prints notices to active window regardless of server"
 __author__ = "Nathan (nathh_)"
 
